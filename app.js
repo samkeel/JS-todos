9719e227a08a74714bf861cb7c6efeaadc592997
Sam <programming@vectors.net.au>
2020-09-17 20:45:03 +0800

    added filter and delete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
     const html = `
@@ -24,3 +25,28 @@ addForm.addEventListener('submit', e => {
     }
 
 });
+
+// delete todos
+// event listener
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+// keyup event
+search.addEventListener('keyup', () => {
+    // Convert to lowercase
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+})
